a3bcdb2787ea16dab8190624fb1c99271b977f06
王雪梅 <wxm112@me.com>
2015-02-19 22:58:09 +1100

    three cats work in the same way

--- dancingcats/dancingcats.js
+++ dancingcats/dancingcats.js
@@ -2,23 +2,43 @@
 var movePixels = 1;
 var delayMs = 5;
 var catTimer = null;
+var div = document.getElementById('info');
 
-var direction = 1;
-
-function catWalk() {
-  var img = document.getElementsByTagName('img')[0];
-  var currentLeft = parseInt(img.style.left);
-  img.style.left = (currentLeft + (movePixels * direction)) + 'px';
-  if (currentLeft > (window.innerWidth-img.width)) {
-    img.style.transform = 'scaleX(-1)';
-    direction = -1;
-  }
-  if (currentLeft <= 0) {
+function flipCat(img) {
+  if (img.style.transform === 'scaleX(-1)') {
     img.style.transform = 'scaleX(1)';
-    direction = 1;
+  } else {
+    img.style.transform = 'scaleX(-1)';
   }
 }
+
+function shiftCat(img) {
+  var currentLeft = parseInt(img.style.left);
+  if(img.direction === undefined) {
+    img.direction = 1;
+  }
+
+  img.style.left = (currentLeft + (movePixels * img.direction)) + 'px';
+  if (currentLeft > (window.innerWidth-img.width)) {
+    flipCat(img);
+    img.direction = -1;
+  }
+  if (currentLeft <= 0) {
+    flipCat(img);
+    img.direction = 1;
+  }
+}
+
+function catWalk() {
+  var images = document.getElementsByClassName('main');
+  [].forEach.call(images, function(img) { 
+    shiftCat(img);
+  });
+}
+
 function startCatWalk() {
+  stopCatWalk();
+  movePixels = 1;
   catTimer = window.setInterval(catWalk, delayMs);
 }
 
@@ -30,16 +50,38 @@ function stopCatWalk() {
   window.clearInterval(catTimer);
 }
 
+function startInfo() {
+  div.innerHTML = "Click on it and the cats shall commence dancing.";
+}
+
+function speedInfo() {
+  div.innerHTML = "Click on it and speed the cats.";
+}
+
+function stopInfo() {
+  div.innerHTML = "Click on it and stop the cats.";
+}
+
+function clearInfo() {
+  div.innerHTML = "";
+}
 
 
 var startButton = document.getElementById('start-button');
 startButton.addEventListener('click', startCatWalk, false);
+startButton.addEventListener('mouseover', startInfo, false);
+startButton.addEventListener('mouseout', clearInfo, false);
 
 var speedButton = document.getElementById('speed-button');
-speedButton.addEventListener('click', speedCatWalk, false)
+speedButton.addEventListener('click', speedCatWalk, false);
+speedButton.addEventListener('mouseover', speedInfo, false);
+speedButton.addEventListener('mouseout', clearInfo, false);
+
 
 var stopButton = document.getElementById('stop-button');
 stopButton.addEventListener('click', stopCatWalk, false);
+stopButton.addEventListener('mouseover', stopInfo, false);
+stopButton.addEventListener('mouseout', clearInfo, false);
 
 
 
