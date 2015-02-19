a51afd85d074a520f6d13c383b96d0637c12480d
王雪梅 <wxm112@me.com>
2015-02-19 12:13:01 +1100

    updated video

--- video/video.js
+++ video/video.js
@@ -1,10 +1,22 @@
-var liarray = document.querySelectorAll('li');
+// var liarray = document.querySelectorAll('li');
 
-for (var i = 0; i < liarray.length; i++) {
-  var link = liarray[i].children
-  var href = link[0].getAttribute('href');
+// for (var i = 0; i < liarray.length; i++) {
+//   var link = liarray[i].children
+//   var href = link[0].getAttribute('href');
+//   var thumbnailUrl = youtube.generateThumbnailUrl(href);
+//   var img = document.createElement('img');
+//   img.setAttribute('src', thumbnailUrl);
+//   liarray[i].appendChild(img);
+//   }
+
+
+var links = document.querySelectorAll('a');
+
+for (var i = 0; i < links.length; i++) {
+  var link = links[i]
+  var href = link.getAttribute('href');
   var thumbnailUrl = youtube.generateThumbnailUrl(href);
   var img = document.createElement('img');
   img.setAttribute('src', thumbnailUrl);
-  liarray[i].appendChild(img);
+  link.appendChild(img);
   }
