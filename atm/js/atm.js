2abeaa51454965e9525bd01627a48796d2bb739d
王雪梅 <wxm112@me.com>
2015-02-22 22:40:32 +1100

    refined atm

--- atm/js/atm.js
+++ atm/js/atm.js
@@ -25,64 +25,67 @@ var accounts = {
   
 };
 
+var changeColor = function () {
+  var array = $('div.balance');
+  $.each(array, function (i, account) {
+    if ($(account).text() === "$0") {
+      $(account).addClass('zero');
+    } else {
+      $(account).removeClass('zero');
+    }
+  });
+};
+
 var common_deposit = function (amount, account) {
   var $newBalance = accounts[account].balance + amount;
   $(accounts[account].element).text('$' + $newBalance); 
   accounts[account].balance = $newBalance;
+  changeColor();
 };
 
 var swapAccount = function (account) {
-  if (account === "checking") {
-    return "savings";
-  } else if (account === "savings") {
-    return "checking";
-  }
+  return account === "checking" ? "savings" : "checking";
 };
 
 var common_withdraw = function(amount, account) {
-  var $thisBalance = accounts[account].balance;
-  var $thatBalance = accounts[swapAccount(account)].balance;
-  if (amount > $thisBalance + $thatBalance) {
-    return
-  } else {
-      if (amount > $thisBalance) {
-        $(accounts[account].element).text('$' + 0);
-        accounts[account].balance = 0;
-        var $newBalance = $thatBalance - (amount - $thisBalance);
-        $(accounts[swapAccount(account)].element).text('$' + $newBalance);
-        accounts[swapAccount(account)].balance = $newBalance;
-      } else {
-          var $newBalance = $thisBalance - amount;
-          $(accounts[account].element).text('$' + $newBalance);
-          accounts[account].balance = $newBalance;
-        }
-    };
+  var currAcct = accounts[account];
+  var otherAcct = accounts[swapAccount(account)];
+  if (amount > currAcct.balance + otherAcct.balance) {
+    return;
+  }
+  currAcct.balance -= amount;
+  if(currAcct.balance < 0) {
+    otherAcct.balance += currAcct.balance;
+    currAcct.balance = 0;
+  }
+
+  $(currAcct.element).text('$' + currAcct.balance);
+  $(otherAcct.element).text('$' + otherAcct.balance);
+  changeColor();
 };
 
+$(document).ready(function () {
+  var helper = function(elementId, accountFunction) {
+      var amount = parseInt($(elementId).val());
+      accountFunction(amount);
+      $(elementId).val("");
+  };
 
+  $('#checkingDeposit').on('click', function() {
+    helper('#checkingAmount', accounts.checking.deposit);
+  });
 
-$('#checkingDeposit').on('click', function() {
-  var $checkingDepositAmount = parseInt($('#checkingAmount').val());
-  accounts.checking.deposit($checkingDepositAmount);
-  $('#checkingAmount').val("");
-});
+  $('#checkingWithdraw').on('click', function() {
+    helper('#checkingAmount', accounts.checking.withdraw);
+  });
 
-$('#checkingWithdraw').on('click', function() {
-  var $checkingWithdrawAmount = parseInt($('#checkingAmount').val());
-  accounts.checking.withdraw($checkingWithdrawAmount);
-  $('#checkingAmount').val("");
-});
+  $('#savingsDeposit').on('click', function() {
+    helper('#savingsAmount', accounts.savings.deposit);
+  });
 
-$('#savingsDeposit').on('click', function() {
-  var $savingsDepositAmount = parseInt($('#savingsAmount').val());
-  accounts.savings.deposit($savingsDepositAmount);
-  $('#savingsAmount').val("");
-});
-
-$('#savingsWithdraw').on('click', function() {
-  var $savingsWithdrawAmount = parseInt($('#savingsAmount').val());
-  accounts.savings.withdraw($savingsWithdrawAmount);
-  $('#savingsAmount').val("");
+  $('#savingsWithdraw').on('click', function() {
+    helper('#savingsAmount', accounts.savings.withdraw);
+  });
 });
 
 
